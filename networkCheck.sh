e2a9cdeed19fe1c0cf017e6cbdfbb9dfec10ee12
Gvln-S <santis.paez2414@outlook.com>
2025-01-21 16:58:16 -0500

    feat: :bricks: start of Gateway connection

--- networkCheck.sh
+++ networkCheck.sh
@@ -2,12 +2,21 @@
 
 if [ -n "$1" ]
 then
+  echo 
+  echo "welcome to networkCheck $USER"
+  echo  
   while getopts :t opt
   do
     case "$opt" in 
       t) echo "local ip: $(hostname -I)"
         ipGateway=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
-        ping $ipGateway -c 3;;
+        echo
+        if [ -n "$(ping $ipGateway -c 3 | grep 'time=' | awk '{print $7}')" ]
+          then
+            echo "your connection with the Gateway is fine"
+          else
+            echo "not local connection with the Gateway"
+        fi;;
       *) echo "$opt is not an option";;
     esac
   done
