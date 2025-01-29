daaca3107da8f4b37556069f03f7894ab241168c
Gvln-S <santis.paez2414@outlook.com>
2025-01-29 11:49:25 -0500

    feat: partial solution for blocks

--- networkCheck.sh
+++ networkCheck.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+function gateway_connection {
+  ip_gateway=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
+  if [ -n "$(ping $ip_gateway -c 3 | grep 'time=' | awk '{print $7}')" ]
+  then
+    echo "your connection with the Gateway is fine"
+  else
+    echo "not local connection with the Gateway"
+  fi
+}
+
+function square_draw {
+  local_ip=$(hostname -I)
+  local count=0
+  while [ -n ${local_ip:$count:$[count + 1]} ]
+  do 
+    echo "1"
+    count=$[count + 1]
+  done
+}
+
+function local_connection {
+  echo "
+ _________________________________
+|               |                 |
+| local ip:     | $(hostname -I)    |
+|_______________|_________________|"
+  square_draw
+}
+
 if [ -n "$1" ]
 then
   echo 
   echo "welcome to networkCheck $USER"
-  echo  
   while getopts :t opt
   do
     case "$opt" in 
-      t) echo "local ip: $(hostname -I)"
-        ipGateway=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
-        echo
-        if [ -n "$(ping $ipGateway -c 3 | grep 'time=' | awk '{print $7}')" ]
-          then
-            echo "your connection with the Gateway is fine"
-          else
-            echo "not local connection with the Gateway"
-        fi;;
+      t) local_connection;;
       *) echo "$opt is not an option";;
     esac
   done
@@ -24,7 +44,4 @@ else
   echo "TODO"
 fi
 
-
-
 exit
-
